b3b012b9172871a4c2b7a76fdf201e18dbd0e22b
Mariusz Kujawski <mariusz@kujaw.ski>
2017-06-06 19:24:20 +0000

    websockets rendering yasss!

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mpu6050 = require('mpu6050');
+var mpu6050 = require('./mpu6050');
 
 // Instantiate and initialize.
 var mpu = new mpu6050();
@@ -7,15 +7,30 @@ mpu.initialize();
 // Test the connection before using.
 mpu.testConnection(function(err, testPassed) {
   if (testPassed) {
-    let i = 0;
+    const { io } = require('./server');
+
+    let n_accX, n_accY, n_accZ;
 
     function chain() {
-      console.log('pomiar ' + i++);
-
       return new Promise((resolve, reject) => {
         mpu.getMotion6(function(err, data){
-          console.log(data);
-          resolve();
+          const [ accX, accY, accZ, gyrX, gyrY, gyrZ ] = data;
+
+          n_accX = 0.6 * (n_accX || accX) + 0.4 * accX;
+          n_accY = 0.6 * (n_accY || accY) + 0.4 * accY;
+          n_accZ = 0.6 * (n_accZ || accZ) + 0.4 * accZ;
+          const output = {
+            accX: n_accX / 16384.0 * 9.81,
+            accY: n_accY / 16384.0 * 9.81,
+            accZ: n_accZ / 16384.0 * 9.81,
+
+            gyrX: Math.round(gyrX / 131.0),
+            gyrY: Math.round(gyrY / 131.0),
+            gyrZ: Math.round(gyrZ / 131.0),
+          };
+
+          io.emit('motion', output);
+          setTimeout(resolve, 16);
         });
       }).then(() => chain());
     }
@@ -24,16 +39,11 @@ mpu.testConnection(function(err, testPassed) {
   }
 });
 
-process.on('SIGTERM', function () {
+function gracefulExit() {
   console.log('go to sleep, sweet prince...');
   // Put the MPU6050 back to sleep.
   mpu.setSleepEnabled(1);
   process.exit(0);
-});
-
-process.on('SIGINT', function () {
-  console.log('go to sleep, sweet prince...');
-  // Put the MPU6050 back to sleep.
-  mpu.setSleepEnabled(1);
-  process.exit(0);
-});
+}
+process.on('SIGTERM', gracefulExit);
+process.on('SIGINT', gracefulExit);
