26da2a7b4773b5b7f8e6a21740af4f0a4275f8a7
Mariusz Kujawski <mariusz@kujaw.ski>
2017-06-12 17:49:57 +0000

    Refactor

---
+++ exit.js
@@ -0,0 +1,3 @@
+const exitHandlers = [];
+
+module.exports = exitHandlers;
--- index.js
+++ index.js
@@ -1,49 +1,12 @@
-var mpu6050 = require('./mpu6050');
-
-// Instantiate and initialize.
-var mpu = new mpu6050();
-mpu.initialize();
-
-// Test the connection before using.
-mpu.testConnection(function(err, testPassed) {
-  if (testPassed) {
-    const { io } = require('./server');
-
-    let n_accX, n_accY, n_accZ;
-
-    function chain() {
-      return new Promise((resolve, reject) => {
-        mpu.getMotion6(function(err, data){
-          const [ accX, accY, accZ, gyrX, gyrY, gyrZ ] = data;
-
-          n_accX = 0.6 * (n_accX || accX) + 0.4 * accX;
-          n_accY = 0.6 * (n_accY || accY) + 0.4 * accY;
-          n_accZ = 0.6 * (n_accZ || accZ) + 0.4 * accZ;
-          const output = {
-            accX: n_accX / 16384.0 * 9.81,
-            accY: n_accY / 16384.0 * 9.81,
-            accZ: n_accZ / 16384.0 * 9.81,
-
-            gyrX: Math.round(gyrX / 131.0),
-            gyrY: Math.round(gyrY / 131.0),
-            gyrZ: Math.round(gyrZ / 131.0),
-          };
-
-          io.emit('motion', output);
-          setTimeout(resolve, 16);
-        });
-      }).then(() => chain());
-    }
-
-    chain();
-  }
-});
+const { io } = require('./server');
+const engineNode = require('./lib/node');
+const engineCpp = require('./lib/cpp');
+const exitHandlers = require('./exit');
 
 function gracefulExit() {
-  console.log('go to sleep, sweet prince...');
-  // Put the MPU6050 back to sleep.
-  mpu.setSleepEnabled(1);
-  process.exit(0);
+    console.log('Shutting down...');
+    exitHandlers.forEach(handler => handler());
 }
+
 process.on('SIGTERM', gracefulExit);
 process.on('SIGINT', gracefulExit);
