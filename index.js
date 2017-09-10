c50e22240ba7aa429371b7dfd962415317caa77a
Mariusz Kujawski <mariusz@kujaw.ski>
2017-09-10 09:36:19 +0000

    Use complementary filter on raw acc/gyro data

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const { io } = require('./server');
-// const engineNode = require('./lib/node');
-// const engineCpp = require('./lib/cpp');
-const engineInvensense = require('./lib/invensense');
+console.log(' - starting sensor node engine...');
+const engineNode = require('./lib/node');
+console.log(' - preparing exit handlers...');
 const exitHandlers = require('./exit');
 
-function gracefulExit() {
-    console.log('Shutting down...');
-    exitHandlers.forEach(handler => handler());
+function gracefulExit(code) {
+    return function () {
+        console.log('Got ' + code + '. Shutting down...');
+        exitHandlers.forEach(handler => handler());
+    }
 }
 
-process.on('SIGTERM', gracefulExit);
-process.on('SIGINT', gracefulExit);
+process.on('SIGTERM', gracefulExit('SIGTERM'));
+process.on('SIGINT', gracefulExit('SIGINT'));
